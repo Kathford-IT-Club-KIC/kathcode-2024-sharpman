95c12f96ad0ded54be36dc28de9affa231188a3b
shashwatthapa <thapashashwat@gmail.com>
2024-06-28 20:10:49 +0545

    donation page completed

---
+++ donateFolder/script.js
@@ -0,0 +1,8 @@
+const btn1 = document.getElementById('button1');
+const btn2 = decoument.getElementById('button2');
+const btn3 = document.getElementById('button3');
+
+
+btn1.addEventListener('click',()=>{
+
+})
